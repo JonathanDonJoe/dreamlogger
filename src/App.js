efb8d7db5f88dd1a45aecf9fb61cae2eae403c80
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-10-19 23:24:44 -0400

    Added AllDreams

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import CreateEntry from './components/CreateEntry/CreateEntry';
+import AllDreams from './components/AllDreams/AllDreams.js';
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Route path='/' component={Home} />
-                <Route path='/Entry' component={CreateEntry} />
+                <Route path='/home' component={Home} />
+                <Route path='/entry' component={CreateEntry} />
+                <Route path='/dreams' component={AllDreams} />
             </div>
 
         </Router>
---
+++ src/components/AllDreams/AllDreams.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AllDreams extends Component {
+    state = {
+
+    }
+    render() { 
+        return (
+            <h1>Every Entry Here</h1>
+        );
+    }
+}
+ 
+export default AllDreams;

8c10af3f3d730e943db91d0bbd105098d6684fa0
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-10-21 12:27:40 -0400

    Connected to firebase database

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as firebase from 'firebase';
 
 import './App.css';
 import Home from './components/Home/Home';
 import CreateEntry from './components/CreateEntry/CreateEntry';
 import AllDreams from './components/AllDreams/AllDreams.js';
 
-function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Route path='/home' component={Home} />
-                <Route path='/entry' component={CreateEntry} />
-                <Route path='/dreams' component={AllDreams} />
-            </div>
+class App extends Component {
+    state = {
+        text:'0'
+    }
 
-        </Router>
-    );
+    componentDidMount() {
+
+    const rootRef = firebase.database().ref().child('Hi');
+    rootRef.on('value', snap => {
+        console.log(snap)
+        this.setState({
+            text: snap.val()
+        })
+    })
+}
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <h1>{this.state.text}</h1>
+                    <Route path='/home' component={Home} />
+                    <Route path='/entry' component={CreateEntry} />
+                    <Route path='/dreams' component={AllDreams} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;

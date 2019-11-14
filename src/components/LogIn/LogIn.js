c7e11aab524ffeb0debeec56ae1daa94cdd3c945
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-11-14 15:51:46 -0500

    Adds callback to /dreams after successful login

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -13,7 +13,7 @@ class LogIn extends Component {
         // Popup signin flow rather than redirect flow.
         signInFlow: 'popup',
         // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-        // signInSuccessUrl: '/signedIn',
+        signInSuccessUrl: '/dreams',
         // We will display Google and Facebook as auth providers.
         signInOptions: [
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,

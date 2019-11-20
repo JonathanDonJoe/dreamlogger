b4da6c343ee7136756d2d804e9d34e0c9599fcda
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-11-20 11:40:41 -0500

    Creates working name frequency check

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,12 +1,62 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Statistics extends Component {
-    state = {  }
-    render() { 
+    state = {
+        // mostCommonPerson: 'None'
+    }
+
+
+    checkFrequency = str => {
+        if(this.props.myDreams.length) {
+            console.log(this.props.myDreams)
+            const containsString = this.props.myDreams.filter(dream => dream.peopleArr.includes(str))
+            console.log(containsString)
+        }
+    }
+
+
+    render() {
+        this.checkFrequency('Jon')
+
+
+
         return (
-            <h1>Statistics</h1>
+            <div className='container'>
+                <h1>Statistics</h1>
+                <table className='highlight centered'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Frequency</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Alvin</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>Alan</td>
+                            <td>3</td>
+                        </tr>
+                        <tr>
+                            <td>Jonathan</td>
+                            <td>8</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
- 
-export default Statistics;
+
+function mapStateToProps(state) {
+    return({
+        auth: state.auth,
+        myDreams: state.myDreams
+    })
+}
+
+export default connect(mapStateToProps, null)(Statistics);

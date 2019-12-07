86abca6271eb399bc5fc132a516c3703022b6e1c
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-12-07 15:39:43 -0500

    isolated specific string from content

--- src/components/SingleDream/SingleDream.js
+++ src/components/SingleDream/SingleDream.js
@@ -8,7 +8,7 @@ class SingleDream extends Component {
         dream: {
             title: '',
             peopleArr: [],
-            conetnts: ''
+            contents: ''
         }
     }
 
@@ -74,23 +74,135 @@ class SingleDream extends Component {
             }
             if (this.state.dream.contents) {
                 dreamContents = this.state.dream.contents;
+
+                const boldName = 'Jed';
+
+                // ATTEMPT 1 //
+
+                // dreamContents = <p>hello <em>my</em> friends</p>
+
+
+
+                // ATTEMPT 2 //
+
+                // let emphasizedContents = 
+                //     dreamContents
+                //         .split(' ')
+                //         .map( word => 
+                //             word === boldName 
+                //                 ? <em>{word}</em>
+                //                 : word
+                //         )
+
+
+                // ATTEMPT 3 //
+
+                // let contentsArr = []
+                // contentsArr.push(emphasizedContents[0])
+                // for (let i=1; i < emphasizedContents.length; i++) {
+                //     contentsArr.push(' ')
+                //     contentsArr.push(emphasizedContents[i])
+                // }
+
+
+                // ATTEMPT 4 //
+                // This works, but also emphasizes the name if its not its own word (bold 'me' will also bold part of 'home')
+
+                // let emphasizedContents = 
+                //     dreamContents
+                //         .split(boldName)
+                //         // .join(<em>{boldName}</em>)
+
+                // let contentsArr = []
+                // contentsArr.push(emphasizedContents[0])
+                // for (let i=1; i < emphasizedContents.length; i++) {
+                //     // contentsArr.push(' ')
+                //     contentsArr.push(<em>{boldName}</em>)
+                //     contentsArr.push(emphasizedContents[i])
+                // }
+
+
+
+                // // ATTEMPT 5 //
+                // // Works for all instances that are surrounded by spaces.  Does not work for all instances attached to a comma or punctuation
+
+                // let emphasizedContents =
+                //     dreamContents
+                //         .split(' ')
+                //         // .join(<em>{boldName}</em>)
+                //         .map(word =>
+                //             word === boldName
+                //                 ? <b>{word}</b>
+                //                 : word
+                //         )
+
+                // let contentsArr = []
+                // contentsArr.push(emphasizedContents[0])
+                // for (let i=1; i < emphasizedContents.length; i++) {
+                //     // contentsArr.push(' ')
+                //     contentsArr.push(' ')
+                //     contentsArr.push(emphasizedContents[i])
+                // }
+
+
+
+
+                // ATTEMPT 6 //
+                // Works using RegExp
+
+                function matchWord(s, word) {
+                    var re = new RegExp( '\\b' + word + '\\b', 'g');
+                    // console.log(re)
+                    // console.log([...s.matchAll(re)])
+                    return [...s.matchAll(re)];
+                }
+
+                function replaceWord(str, index, word) {
+                    let newStr = str;
+                    newStr = str.slice(0,index) + word + str.slice(index+word.length-1)
+
+                    return newStr
+                }
+
+                let matchedArr = matchWord(dreamContents, boldName)
+                let emphasizedContents = dreamContents
+                
+                for (let i=matchedArr.length-1; i>=0; i--){
+                    // console.log('ran')
+                    emphasizedContents = replaceWord(emphasizedContents, matchedArr[i].index, 'Joel')
+                }
+
+
+
+
+
+
+
+                // console.log(contentsArr)
+                // console.log(emphasizedContents)
+                dreamContents = emphasizedContents
+                // dreamContents = contentsArr
+
+                // console.log(dreamContents)
+
             }
         }
 
         let theDream = this.state.dream
-            ? 
-                <div className='row'>
-                    <h1>{dreamTitle}</h1>
-                    <h5>{dreamDate}</h5>
-                    <h5>People: {dreamPeople}</h5>
-                    <p>{dreamContents}</p>
-                </div>
-            : 
-                <h1>Dream not found</h1>
+            ?
+            <div className='row'>
+                <h1>{dreamTitle}</h1>
+                <h5>{dreamDate}</h5>
+                <h5>People: {dreamPeople}</h5>
+                {/* <p>{dreamContents}</p> */}
+                <p>{dreamContents}</p>
+            </div>
+            :
+            <h1>Dream not found</h1>
         return (
             <div>
                 <div className='container single-dream-container white-text grey darken-2 z-depth-2'>
-                {theDream}
+                    {theDream}
                 </div>
             </div>);
     }

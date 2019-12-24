c18fa0d49aa09e9896ea0cf757f9055fd2a4b848
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-12-24 16:53:08 -0500

    Creates cards for all dreams in allDreams

--- src/components/PublicDreams/PublicDreams.js
+++ src/components/PublicDreams/PublicDreams.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+
+import DreamCard from '../DreamCard/DreamCard';
+import SearchBar from '../SearchBar/SearchBar';
+
 class PublicDreams extends Component {
     state = {
 
     }
     render() {
-        console.log(this.props.allDreams)
+        let msg = <h1>Dreams</h1>
+
+        let dreamCards = this.props.allDreams.map((dreamItem, i) => <DreamCard dream={dreamItem.dream} dreamId={i} key={i} />)
+
+        // let dreamCards = this.state.filterBy
+        //     ? this.filterDreams(this.props.allDreams).map((dream, i) => <DreamCard dream={dream} dreamId={i} key={i} />)
+        //     : this.props.allDreams.map((dream, i) => <DreamCard dream={dream} dreamId={i} key={i} />)
+
         return (
-            <h1>Public Dreams</h1>
+            <div>
+                {msg}
+                <div className='container card-container'>
+                    <div className='row'>
+                        < SearchBar submitSearch={this.submitSearch}/>
+                        {dreamCards}
+                    </div>
+                </div>
+            </div>
         );
     }
 }

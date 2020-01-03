b642072c43aeb0279a3c737848e6462121d2fd8d
Jonathan Joe <JonathanDonJoe@gmail.com>
2020-01-03 13:27:25 -0500

    Cleaned code

--- src/components/SingleDream/SingleDream.js
+++ src/components/SingleDream/SingleDream.js
@@ -16,20 +16,9 @@ class SingleDream extends Component {
     }
 
     componentDidMount() {
-        // This runs before this.props.myDreams is updated by the store
-        // console.log('componentDidMount')
-        // console.log(this.props)
         if (this.props.myDreams.length) {
-            // let dreamIndex = Object.keys(this.props.myDreams).find(key => this.props.myDreams[key].dreamKey === this.props.match.params.dreamId)
-            // console.log('owner')
-            // this.setState({
-            //     dream: this.props.myDreams[dreamIndex]
-            // })
             this.checkOwnership(this.props.myDreams, true)
         } else {
-            // console.log('set dream without ownership')
-            // console.log(this.props.match.params.dreamId)
-            // console.log(this.props.allDreams)
             if (this.props.allDreams.length) {
                 this.checkNotOwnership(this.props.allDreams, false)
             }
@@ -39,30 +28,11 @@ class SingleDream extends Component {
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.myDreams !== this.props.myDreams) {
             if (this.props.myDreams.length) {
-                //     let dreamIndex = Object.keys(this.props.myDreams).find(key => this.props.myDreams[key].dreamKey === this.props.match.params.dreamId)
-                //     console.log(dreamIndex)
-                //     console.log('owner')
-                //     if (dreamIndex) {
-                //         this.setState({
-                //             dream: this.props.myDreams[dreamIndex]
-                //         })
-                //     }
                 this.checkOwnership(this.props.myDreams, true)
             }
         }
         if (prevProps.allDreams !== this.props.allDreams) {
             if (this.props.allDreams.length) {
-                // console.log('update setDream without ownership')
-                // console.log(this.props.match.params.dreamId)
-                // console.log(this.props.myDreams)
-                // console.log(this.props.allDreams)
-                // let dreamIndex = Object.keys(this.props.allDreams).find(key => this.props.allDreams[key].dreamKey === this.props.match.params.dreamId)
-                // console.log(dreamIndex)
-
-                // // sanitize and randomize dream here.  then setState
-                // this.setState({
-                //     dream: this.props.allDreams[dreamIndex]
-                // })
                 this.checkOwnership(this.props.allDreams, false)
             }
         }
@@ -86,26 +56,13 @@ class SingleDream extends Component {
                     })
                 }
             } else if (!dreamIndex && isOwnedDream) {
-                // console.log('look here')
                 this.checkOwnership(this.props.allDreams, false)
             }
         }
     }
 
-    checkNotOwnership = (dreamArr) => {
-        if (dreamArr.length) {
-            let dreamIndex = Object.keys(dreamArr).find(key => dreamArr[key].dreamKey === this.props.match.params.dreamId)
-            console.log(dreamIndex)
-            // sanitize and randomize dream here.  then setState
-            this.setState({
-                dream: dreamArr[dreamIndex]
-            })
-        }
-    }
-
 
     render() {
-
         console.log(this.state)
         console.log(this.props.auth)
         let dreamTitle = 'Untitled'
@@ -115,9 +72,6 @@ class SingleDream extends Component {
         let dreamPeopleArr = [];
 
         if (this.state.dream) {
-            // console.log(this.state.dream.peopleArr)
-
-
             if (!this.state.owner) {
 
                 if (this.state.dream.date) {
@@ -128,63 +82,27 @@ class SingleDream extends Component {
                     dreamTitle = this.state.dream.title;
                 }
                 if (this.state.dream.peopleArr) {
-                    // dreamPeople = this.state.dream.peopleArr.filter(person => person).join(', ');
                     dreamPeopleArr = this.state.dream.peopleArr.filter(person => person).map(person => [person, faker.name.findName()])
                     dreamPeople = dreamPeopleArr.map(personArr => personArr[1]).join(', ');
-                    // console.log(dreamPeopleArr)
-                    // console.log(dreamPeople)
                 }
                 if (this.state.dream.contents) {
-                    // dreamContents = this.state.dream.contents;
-
-                    // const boldName = 'Jed';
-                    // const replacementName = faker.name.findName();
-
-
-                    // let matchedArr = matchWord(dreamContents, boldName)
-                    // let emphasizedContents = dreamContents
-
-                    // for (let i = matchedArr.length - 1; i >= 0; i--) {
-                    //     emphasizedContents = replaceWord(emphasizedContents, matchedArr[i].index, replacementName);
-                    // }
-
-
                     // This does a 1:1 replacement.  It does not replace partial names
                     let emphasizedContents = this.state.dream.contents;
                     for (let i = 0; i < dreamPeopleArr.length; i++) {
-
                         let boldName = dreamPeopleArr[i][0];
                         let replacementName = dreamPeopleArr[i][1];
                         let matchedArr = matchWord(emphasizedContents, boldName)
-                        // console.log(matchedArr)
                         for (let i = matchedArr.length - 1; i >= 0; i--) {
                             emphasizedContents = replaceWord(emphasizedContents, boldName.length, matchedArr[i].index, replacementName);
                         }
-                        // console.log(emphasizedContents)
                         let boldFirstName = dreamPeopleArr[i][0].split(' ')[0]
-                        // console.log(boldFirstName);
                         let replacementFirstName = dreamPeopleArr[i][1].split(' ')[0];
-                        // console.log(replacementFirstName);
                         let matchedArr2 = matchWord(emphasizedContents, boldFirstName)
-                        // console.log(matchedArr2);
-                        // console.log(matchedArr2.length);
-                        // if (matchedArr2.length) {
                         for (let i = matchedArr2.length - 1; i >= 0; i--) {
                             emphasizedContents = replaceWord(emphasizedContents, boldFirstName.length, matchedArr2[i].index, replacementFirstName);
                         }
-                        // console.log(emphasizedContents)
-                        // }
                     }
-
-
-
-                    // console.log(contentsArr)
-                    // console.log(emphasizedContents)
                     dreamContents = emphasizedContents
-                    // dreamContents = contentsArr
-
-                    // console.log(dreamContents)
-
                 }
             }
             else {
